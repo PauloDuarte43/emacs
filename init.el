b14d5afbab32fd9370a5a002851e2d45cf207a19
Paulo Duarte <pauloduarte@cwi.com.br>
2017-09-04 11:01:21 -0300

    added jedi

--- init.el
+++ init.el
@@ -5,11 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("6c0a087a4f49c04d4002393ffd149672f70e4ab38d69bbe8b39059b61682b61c" "251348dcb797a6ea63bbfe3be4951728e085ac08eee83def071e4d2e3211acc3" default)))
+    ("e3bf16af35586816b824bea36188215319b1cceb208d3518700d876c4c1a9cc7" "6c0a087a4f49c04d4002393ffd149672f70e4ab38d69bbe8b39059b61682b61c" "251348dcb797a6ea63bbfe3be4951728e085ac08eee83def071e4d2e3211acc3" default)))
  '(org-todo-keywords (quote ((sequence "TODO" "DOING" "DONE"))))
  '(package-selected-packages
    (quote
-    (rainbow-mode web-mode mustang-theme auto-package-update auto-complete airline-themes)))
+    (jedi rainbow-mode web-mode mustang-theme auto-package-update auto-complete airline-themes)))
  '(safe-local-variable-values (quote ((encoding . utf-8))))
  '(show-paren-mode t))
 (custom-set-faces
@@ -80,3 +80,5 @@
       (mapc #'kill-buffer killed)
       (message "%d buffers killed: %s" (length killed) killed))))
 (global-set-key (kbd "C-x K") 'ask-and-kill-unmodified-buffers)
+
+(add-hook 'python-mode-hook 'jedi:setup)
